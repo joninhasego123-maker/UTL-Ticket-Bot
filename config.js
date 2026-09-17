45f9ac43a88f41a5da1a73b062ac3ac8fcc6be36
joninhasego123-maker <joninhasego123@gmail.com>
2026-09-17 18:47:39 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 module.exports = {
     TOKEN: process.env.TOKEN,
 
+    // =========================
+    // TICKETS
+    // =========================
     TICKET_CATEGORY_ID: "1543584539467587585",
-
     STAFF_ROLE_ID: "1543595314576101447",
+    TICKET_PANEL_CHANNEL_ID: "1543584583113777212",
 
-    TICKET_PANEL_CHANNEL_ID: "1543584583113777212"
+    // =========================
+    // CONTRACT
+    // =========================
+    CONTRACT_CHANNEL_ID: "1543583112196268134",
+    CONTRACT_LOG_CHANNEL_ID: "1543911670659682365"
 };
